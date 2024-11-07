27fe7c8b4f830f22e9ba80819e2f81ac4bff6cf9
sidhart25 <navneetnishchal1420@gmail.com>
2024-11-07 15:49:36 +0530

    req.pipe thing learnt

---
+++ learningFromBusBoySC.js
@@ -0,0 +1,3 @@
+// When you send a form - data request(like the example you've shown) and pipe the request (req)
+// to a writable stream, only the body content of the request flows through the stream,
+// not the headers or other metadata
